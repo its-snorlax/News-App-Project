a82470b25e4e0c3c2f53caa5c83c4bc6fa60ed53
JuniorJainsahab <p.jain3322@gmail.com>
2020-11-23 13:16:17 +0530

    fixing networking issue for API level higher >= 28

--- app/src/main/java/com/juniorjainsahab/newsapp/network/ApiParam.kt
+++ app/src/main/java/com/juniorjainsahab/newsapp/network/ApiParam.kt
@@ -2,7 +2,7 @@ package com.juniorjainsahab.newsapp.network
 
 class ApiParam {
     companion object {
-        const val BASE_URL = "http://newsapi.org";
+        const val BASE_URL = "https://newsapi.org";
         const val API_KEY = "7cb8b3ac7c4745b5a8ba6f50becde20d"
     }
 }
