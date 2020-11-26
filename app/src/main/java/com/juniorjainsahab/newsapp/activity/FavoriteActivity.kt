ce3ff8e4e53da568c0d275e01ea293f6de254687
JuniorJainsahab <p.jain3322@gmail.com>
2020-11-26 23:45:15 +0530

    adding layouts if 0 favorite items were found.

--- app/src/main/java/com/juniorjainsahab/newsapp/activity/FavoriteActivity.kt
+++ app/src/main/java/com/juniorjainsahab/newsapp/activity/FavoriteActivity.kt
@@ -3,10 +3,14 @@ package com.juniorjainsahab.newsapp.activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.juniorjainsahab.newsapp.R.id.list_view
+import com.juniorjainsahab.newsapp.R.id.no_data_message
 import com.juniorjainsahab.newsapp.R.layout.activity_favorite
 import com.juniorjainsahab.newsapp.adapter.ArticlesAdapter
 import com.juniorjainsahab.newsapp.asyncTask
@@ -18,6 +22,8 @@ import com.juniorjainsahab.newsapp.model.Articles
 class FavoriteActivity : AppCompatActivity(), OnExecuteListener {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var noFavoriteMessage: LinearLayout
+
     private lateinit var favArticles: List<ArticlesEntity>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +33,8 @@ class FavoriteActivity : AppCompatActivity(), OnExecuteListener {
         supportActionBar?.title = "Favorite"
 
         recyclerView = findViewById(list_view)
+        noFavoriteMessage = findViewById(no_data_message)
+
         asyncTask({
             favArticles = AppDB.getDbInstance(this).articlesDao().getAll()
         }, this)
@@ -40,12 +48,18 @@ class FavoriteActivity : AppCompatActivity(), OnExecuteListener {
     override fun onQuerySuccess() {
         val articles =
             favArticles.map { Articles(it.title, it.urlToImage, it.description, it.url) }
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = ArticlesAdapter(articles,
-            View.OnClickListener {
-                val activityContent = articles[recyclerView.getChildLayoutPosition(it)]
-                openArticleDetailActivity(activityContent)
-            })
+
+        if (articles.isNotEmpty()) {
+            noFavoriteMessage.visibility = GONE
+            recyclerView.visibility = VISIBLE
+
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            recyclerView.adapter = ArticlesAdapter(articles,
+                View.OnClickListener {
+                    val activityContent = articles[recyclerView.getChildLayoutPosition(it)]
+                    openArticleDetailActivity(activityContent)
+                })
+        }
     }
 
     private fun openArticleDetailActivity(activityContent: Articles) {
